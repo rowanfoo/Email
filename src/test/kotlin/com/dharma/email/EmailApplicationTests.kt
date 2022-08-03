36910f7fb15c5c77a9cf63e9c7a6ddf88c5c5c13
rowan <rowanfoo@gmail.com>
2022-08-03 18:51:27 +0800

    feature: can test Unirest to ownself

--- src/test/kotlin/com/dharma/email/EmailApplicationTests.kt
+++ src/test/kotlin/com/dharma/email/EmailApplicationTests.kt
@@ -1,9 +1,13 @@
 package com.dharma.email
 
 import com.dharma.email.service.EmailService
+import com.mashape.unirest.http.JsonNode
+import com.mashape.unirest.http.Unirest
+import com.sun.deploy.net.HttpResponse
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+
 
 @SpringBootTest
 class EmailApplicationTests {
@@ -15,5 +19,23 @@ class EmailApplicationTests {
     fun contextLoads() {
         emailService.sendMail("rowanfoo@gmail.com","Hello World" , "Good fortune to me and family")
     }
+    @Test
+    fun sendHtml() {
+
+        emailService.sendMail("rowanfoo@gmail.com","Hello World" , "<html><h1>some text !</h1></html>")
+//        emailService.sendMail("rowanfoo@gmail.com","Hello World" , html)
+
+    }
+    @Test
+    fun sendPost() {
+
+        var jsonResponse = Unirest.post("http://localhost:8080")
+            .body("{\"name\":\"Sam Baeldung\", \"city\":\"viena\"}")
+            .asString()
+        println(jsonResponse)
+
+    }
+
+
 
 }
